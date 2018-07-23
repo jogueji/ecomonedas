571cc719bc85b13e6e0ff87226e5faeb765f9537
Unknown <jcsoto.1989@gmail.com>
2018-07-23 17:28:27 -0500

    Cambios para collection center

---
+++ resources/views/collectioncenter.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('title', 'Centros de Acopio')
+@section('content')
+  <section class="probootstrap-section">
+    <div class="container">
+      <div class="probootstrap-section-heading text-center mb50 probootstrap-animate">
+        <h2 class="heading">Centros de Acopio</h2>
+      </div>
+    </div>
+  </section>
+@endsection
