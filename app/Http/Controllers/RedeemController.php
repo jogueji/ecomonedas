f0580656fa95cb897a8406e2dc41f9735e5f31a8
Unknown <yo.nathan421@gmail.com>
2018-08-08 06:48:45 -0600

    Cambios

--- app/Http/Controllers/RedeemController.php
+++ app/Http/Controllers/RedeemController.php
@@ -3,8 +3,97 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
+use App\Recyclablematerial;
+use App\User;
+use Auth;
+use App\Redeem;
+use App\Detailredemption;
+use Illuminate\Session\Store;
 
 class RedeemController extends Controller
 {
-    //
+  public function getIndex(Store $session){
+    $details = collect([1]);
+    $session->put('id',1);
+    $session->put('details',$details);//guarda en el arreglo en session
+    $materials=Recyclablematerial::all();
+    $users=User::all();
+    //$users=User::where('rol_id', 3)->orderBy('email','asc');//no cargan los usuarios
+    if(count($materials)==0){
+      return redirect()->route('index')->with('message', 'No existen materiales');
+    }
+    return view('redeem',['users'=>$users,'materials'=>$materials,]);
+  }
+
+  public function addDetail(Store $session){
+    $details= $session->get('details');
+    $id=$session->get('id')+1;
+    array_push($details, $id);
+    $materials=Recyclablematerial::all();
+    $session->put('details', $details);
+    $session->put('id', $id);
+    return response()->json(['materials'=>$materials,'id'=>$id]);
+  }
+
+  public function deleteDetail($id, Store $session){
+    $details= $session->get('details');
+    array_pull($details,$id);
+    $session->put('details', $details);
+    return response()->json($id);
+  }
+
+  public function redeem(Request $request,Store $session){
+    $details= $session->get('details');//validaciones
+    $this->validate($request, [
+      'user' => 'required'
+    ]);
+    if(count($details)==0){
+      $validator = Validator::make([], []);
+      $validator->errors()->add('user','Asigne al menos un material a canjear');
+      return redirect()->route('redeem.index')->withErrors($validator)->withInput();
+    }
+    foreach($details as $detail){
+      $value='kg'.intval($detail);
+      if($request[$value]==0){
+        $validator = Validator::make([], []);
+        $validator->errors()->add('user','Asigne un peso mayor a 0');
+        return redirect()->route('redeem.index')->withErrors($validator)->withInput();
+      }
+    }
+
+    $redeem=new Redeem(['total'=>0]);
+    $userAdmin=Auth::user();
+    $redeem->userAdmin()->associate($userAdmin);
+    $redeem->collectioncenter_id=$userAdmin->collectioncenter_id;
+    $redeem->userClient()->associate(User::find($request['user']));
+    $redeem->save();
+
+    $list=collect();
+    foreach($details as $detail){
+      $value='material'.intval($detail);
+      $material=Recyclablematerial::find($request[$value]);
+      $value='material'.intval($detail);
+      $kg=$request[$value];
+      if($list->where('recyclablematerial_id',$material->id)->first()==null){
+        $list->push(new Detailredemption([
+          'subtotal'=>$kg*$material->price,
+          'kilograms'=>$kg
+        ]));
+        $list->last()->recyclablematerial()->associate($material);
+        $list->last()->redeem()->associate($redeem);
+        $list->last()->save();
+      }
+      else{
+        $list->where('recyclablematerial_id',$material->id)->first()->kilograms+=$kg;
+        $list->where('recyclablematerial_id',$material->id)->first()->total+=$kg*$material;
+        $list->where('recyclablematerial_id',$material->id)->first()->save();
+      }
+      $redeem->total+=$list->last()->subtotal;
+    }
+
+    $redeem->save();//guarda el nuevo total
+    return redirect()->route('index')->with('message', 'Materiales canjeados');
+  }
+
 }
--- resources/views/redeem.blade.php
+++ resources/views/redeem.blade.php
@@ -1,4 +1,82 @@
 @extends('layouts.master')
+@section('title', 'Canjear material')
+@section('content')
+  <section class="probootstrap-section">
+    @include('partials.errors')
+    <div class="container">
+      <div class="probootstrap-section-heading text-center mb50 probootstrap-animate">
+        <h2 class="heading">Canjeo de materiales</h2>
+      </div>
+      <form action="{{ route('redeem.create') }}" method="post" class="probootstrap-form mb60">
+        @csrf
+        <div class="col-md-8">
+          <div class="col-md-12">
+            <div class="form-group">
+                <label for="user">Usuario</label>
+                <select id="user" name="user" class="form-control" style="-webkit-appearance: menulist;">
+                  @foreach ($users as $user)
+                  <option value="{{$user->id}}" {{old('user')==$user->id?'selected':''}}>{{$user->email}}-{{$user->name}} {{$user->lastname}} {{$user->lastname1}}</option>
+                  @endforeach
+                </select>
+            </div>
+          </div>
+          <div class="col-md-12">
+              <h4 class="sub-heading">Lista de materiales</h4>
+          </div>
+          <div id="list">
+            <div class="col-md-12 detail1">
+              <div class="col-md-4">
+              <div class="form-group">
+                  <label for="material1">Material</label>
+                  <select id="material1" name="material1" class="form-control" style="-webkit-appearance: menulist;">
+                    @foreach ($materials as $material)
+                    <option value="{{$material->id}}" {{old('material1')==$material->id?'selected':''}}>{{$material->name}}</option>
+                    @endforeach
+                  </select>
+              </div>
+            </div>
+              <div class="col-md-4">
+              <div class="form-group">
+                  <label for="kg1">Kilogramos</label>
+                  <input id="kg1" type="number" placeholder="0.0" step="0.01" min="0" class="form-control" name="kg1" value="{{ old('kg1') }}">
+              </div>
+            </div>
+              <div class="col-md-4">
+              <br>
+              <div class="form-group">
+                <a href="#" data-id="1" style="border: 1px solid red;background-color:red;" class="btn btn-primary delete-modal">
+                  Eliminar material
+                </a>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="form-group">
+              <a href="#" style="border: 1px solid blue;background-color:blue;" class="btn btn-primary create-modal">
+                Añadir material
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6" align="right">
+            <div class="form-group">
+              <button type="submit" class="btn btn-primary">Canjear</button>
+            </div>
+          </div>
+          <div class="col-md-6" align="left">
+            <a class="btn btn-primary" href="{{ route('index')}}">Cancelar</a>
+          </div>
+        </div>
+        <div class="col-md-4" align="center">
+          <img src={{asset('img/reciclaje.jpg')}} height="100%" width="100%">
+        </div>
+      </form>
+
+    </div>
+  </section>
+  <script type="text/javascript" src="{{ URL::to('js/redeem.js') }}"></script>
+@endsection
+{{--@extends('layouts.master')
 @section('title', 'Canjeo de materiales')
 @section('content')
 <div class="row">
@@ -20,15 +98,15 @@
         </th>
       </tr>
       @csrf
-      @foreach ($plataforma as $item)
+      @foreach ($materials as $item)
         <tr class="plataforma{{$item->id}}">
-          <td>{{ $item->nombre }}</td>
+          <td>{{ $item->name }}</td>
           <td>{{ $item->created_at }}</td>
           <td>
-            <a href="#" class="show-modal btn btn-info" data-id="{{$item->id}}" data-nombre="{{$item->nombre}}" >
+            <a href="#" class="show-modal btn btn-info" data-id="{{$item->id}}" data-nombre="{{$item->name}}" >
               <i class="far fa-eye"></i>
             </a>
-            <a href="#" class="edit-modal btn btn-warning" data-id="{{$item->id}}" data-nombre="{{$item->nombre}}" >
+            <a href="#" class="edit-modal btn btn-warning" data-id="{{$item->id}}" data-nombre="{{$item->name}}" >
               <i class="far fa-edit"></i>
             </a>
 
@@ -37,46 +115,10 @@
       @endforeach
     </table>
   </div>
-  {{$plataforma->links()}}
-</div>
-{{--Inicio Formulario Modal Nuevo--}}
-<div id="create" class="modal fade" role="dialog">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Nueva Plataforma</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <div class="alert alert-danger" style="display:none"></div>
-
-        <form class="form-horizontal" role="form">
-          <div class="form-group row add">
-            <label class="control-label col-sm-2" for="nombre">Nombre:</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="nombre" name="nombre"
-              placeholder="Nombre de la plataforma" required>
-              <p class="error text-center alert alert-danger hidden"></p>
-            </div>
-          </div>
-        </form>
-      </div>
-          <div class="modal-footer">
-            <button class="btn btn-warning" type="submit" id="add">
-              <span class="glyphicon glyphicon-plus"></span>Guardar Plataforma
-            </button>
-            <button class="btn btn-warning" type="button" data-dismiss="modal">
-              <span class="glyphicon glyphicon-remobe"></span>Cerrar
-            </button>
-          </div>
-    </div>
-  </div>
+  {{$materials->links()}}
 </div>
 
-{{--Fin Formulario Modal Nuevo--}}
-{{-- Formulario Modal Detalle--}}
+{{-- Formulario Modal Detalle--
 <div id="show" class="modal fade" role="dialog">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -100,7 +142,7 @@
   </div>
 </div>
 {{-- Fin Formulario Modal Detalle--}}
-{{-- Formulario Modal Editar--}}
+{{-- Formulario Modal Editar--
 <div id="myModal"class="modal fade" role="dialog">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
@@ -138,5 +180,7 @@
       </div>
     </div>
 </div>
-{{-- Fin Formulario Modal Editar--}}
+{{-- Fin Formulario Modal Editar--
+
 @endsection
+--}}
